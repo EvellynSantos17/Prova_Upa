7784016981ecffeb83607a70a28dd40004ac99c9
EvellynSantos17 <evellynaparecida0@gmail.com>
2025-08-07 23:16:01 -0300

    FIX: ajustando o  espacos

--- Back/app/Http/Controllers/SetorController.php
+++ Back/app/Http/Controllers/SetorController.php
@@ -8,11 +8,19 @@ class SetorController extends Controller
 {
     private Setor $setor;
 
+    /**
+     * SetorController constructor.
+     * @param Setor $setor
+     */
     public function __construct(Setor $setor)
     {
         $this->setor = $setor;
     }
 
+    /**
+     * Método para listar todos os setores
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
         $setores = $this->setor
