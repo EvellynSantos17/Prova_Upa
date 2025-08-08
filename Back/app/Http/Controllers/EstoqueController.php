265e5ed8689f195a1b36d52b3c271df542b859c9
EvellynSantos17 <evellynaparecida0@gmail.com>
2025-08-07 21:15:53 -0300

    FIX: ajustando o estoque

--- Back/app/Http/Controllers/EstoqueController.php
+++ Back/app/Http/Controllers/EstoqueController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Estoque;
+use App\Models\Itens;
 use Illuminate\Http\Request;
 
 class EstoqueController extends Controller
 {
     protected $estoque;
+    protected $item;
 
-    public function __construct(Estoque $estoque)
+    public function __construct(Estoque $estoque , Itens $item)
     {
         $this->estoque = $estoque;
+        $this->item = $item;
     }
 
    public function index()
@@ -38,23 +41,45 @@ class EstoqueController extends Controller
         return response()->json($response->values());
     }
 
-    public function lista()
-    {
-        $estoques = Estoque::with('item:id,nome,descricao,codigo')
-            ->get();
+   
 
-        $response = $estoques->map(function ($estoque) {
-            return [
-                'nome'       => $estoque->item->nome ?? null,
-                'descricao'  => $estoque->item->descricao ?? null,
-                'codigo'     => $estoque->item->codigo ?? null,
-                'estoque'    => [
-                    'quantidade' => $estoque->quantidade,
-                ],
-                'id' => $estoque->id,
-            ];
-        });
+  public function destroy($id)
+{
+    $item = $this->item->find($id);
 
-        return response()->json($response->values());
+    if (!$item) {
+        return response()->json(['error' => 'Item não encontrado'], 404);
     }
+
+    $itensComMesmoNome = $this->item->where('nome', $item->nome)->get();
+
+    if ($itensComMesmoNome->isEmpty()) {
+        return response()->json(['error' => 'Nenhum outro item com mesmo nome encontrado'], 404);
+    }
+
+    $estoques = Estoque::whereIn('item_id', $itensComMesmoNome->pluck('id'))->get();
+
+    if ($estoques->isEmpty()) {
+        return response()->json(['error' => 'Nenhum estoque encontrado para os itens com mesmo nome'], 404);
+    }
+
+    $estoqueParaAlterar = $estoques->sortByDesc('quantidade')->first();
+
+    if ($estoqueParaAlterar->quantidade > 0) {
+        $estoqueParaAlterar->quantidade -= 1;
+        $estoqueParaAlterar->save();
+    }
+
+    if ($estoqueParaAlterar->quantidade <= 0) {
+        $estoqueParaAlterar->delete();
+    }
+
+    $item->delete();
+
+    return response()->json(['message' => 'Item deletado e estoque atualizado com sucesso.']);
+}
+
+
+
+
 }
